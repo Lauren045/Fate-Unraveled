604686d6d378f775629d7b03eac3739822db5d0c
ffd24 <ffd24@tux5.cci.drexel.edu>
2025-02-08 20:36:41 -0500

    Dialogue progression added

---
+++ game.js
@@ -0,0 +1,34 @@
+let dialogues = [];
+let dialogueIndex = 0;
+
+//Load the dialogue from dialogue.json
+async function loadDialogue() {
+    const response = await fetch("JSON/dialogue.json");
+    const data = await response.json();
+    dialogues = data.dialogues;
+    showDialogue();
+}
+
+function showDialogue() {
+    const dialogueTextElement = document.getElementById("dialogueText");
+    dialogueTextElement.innerText = dialogue[dialogueIndex];
+}
+
+function dialogueProgression() {
+    dialogueIndex++;
+
+    if (dialogueIndex < dialogues.length) {
+        showDialogue();
+    }
+}
+
+function handleInput(event) {
+    if (event.key === " " || event.type === "click") {
+        dialogueProgression();
+    }
+}
+
+document.addEventListener('keydown', userInput);
+document.addEventListener('click', userInput);
+
+loadDialogue();
