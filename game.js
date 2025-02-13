0916f37f6739851149d780d7e61d2bd2611d4ca7
ffd24 <ffd24@tux5.cci.drexel.edu>
2025-02-13 11:21:20 -0500

    Added bg + chara

--- game.js
+++ game.js
@@ -1,1 +1,18 @@
+const background = document.getElementById("backgroundImage");
+const characters = document.getElementById("characterImages");
 
+function changeBackground(imageFile) {
+    background.src = `assets/IMG/${imageFile}`;
+}
+
+function changeCharacter(imageFile) {
+    if (imageFile) {
+        characters.src = `assets/IMG/${imageFile}`;
+        characters.style.display = "block";
+    } else {
+        characters.style.display = "none";
+    }
+}
+
+changeBackground("bg.jpg")
+changeCharacter("def.png")
