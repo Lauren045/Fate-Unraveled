0ae797c9d2f4cf650f8aa3ed56d4de0879feb5fa
ffd24 <ffd24@tux3.cci.drexel.edu>
2025-04-09 11:23:12 -0400

    speed line stuff, manually making a gif so that i can finish this

--- game.js
+++ game.js
@@ -35,8 +35,12 @@ function triggerFX(fxType) {
         sceneEffect.style.animation = "fadeOut 3s ease-in-out";
     }
 
-    //else if (fxType === "speedLines") {
-        //sceneEffect.style.animation = "speedLines 3s ease-in-out";
+    //else if (fxType === "speedBurst") {
+        //const burst = document.getElementById("speedBurst");
+        //burst.style.display = "block";
+        //setTimeout(() => {
+            //burst.style.display = "none";
+        //}, 1000);
     //}
 
     setTimeout(() => {
